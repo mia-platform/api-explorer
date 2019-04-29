b901092edac7d77287aac42811d99262c3b83c5d
Federico Maggi <federico.maggi@mia-platform.eu>
2019-04-29 16:43:47 +0200

    using PureComponent instead of shouldComponentUpdate in custom Select component

--- packages/api-explorer/src/components/Select/index.js
+++ packages/api-explorer/src/components/Select/index.js
@@ -1,18 +1,7 @@
 import React from 'react'
 import { Select as SelectComponent } from 'antd'
 
-export default class Select extends React.Component {
-
-  shouldComponentUpdate(nextProps) {
-    const {value} = this.props
-    if (nextProps.value === undefined || value === undefined) {
-      return true
-    }
-    if (nextProps.value !== value) {
-      return true
-    }
-    return false
-  }
+export default class Select extends React.PureComponent {
 
   render() {
     const { options, value, onChange } = this.props
