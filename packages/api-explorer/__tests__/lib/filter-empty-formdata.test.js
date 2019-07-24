79dddf0005c51e316962c50523862468976316a7
Federico Maggi <federico.maggi@mia-platform.eu>
2019-07-24 14:24:29 +0200

    fix(AC-1123): Fixed filtering of numbers and objects

--- packages/api-explorer/__tests__/lib/filter-empty-formdata.test.js
+++ packages/api-explorer/__tests__/lib/filter-empty-formdata.test.js
@@ -1,40 +1,120 @@
 import filterEmptyFormData from '../../src/lib/filter-empty-formdata'
 
-const data = {
-  // eslint-disable-next-line global-require
-  "schema": require('./sample-schema.json'),
-  "formData": {
-    "body": {
-      "$set": {
-        "position": [
-          123,
-          456
-        ],
-        "image.$.replace": {
-          key: undefined,
+describe('filterEmptyFormData', () => {
+  it('filters all empty objects or objects with all undefined', () => {
+    const input = {
+      body: {
+        $set: {
+          position: [123, 456],
+          "image.$.replace": {
+            key: undefined,
+          },
+          "image.$.merge": {}
         },
-        "image.$.merge": {}
-      },
-      "$unset": {
-        anotherKey: undefined
-      },
-      "$inc": {},
-      "$mul": {},
-      "$currentDate": {},
-      "$push": {
-        "image": {}
+        $unset: {
+          anotherKey: undefined
+        },
+        $inc: {},
+        $mul: {},
+        $currentDate: {},
+        $push: {
+          image: {}
+        }
       }
     }
-  }
-}
-
-test('properly filters data from example', () => {
-  const result = filterEmptyFormData(data.formData, data.schema)
-  expect(result).toEqual({
-    body: {
-      $set: {
-        position: [123, 456],
+    const result = filterEmptyFormData(input)
+    expect(result).toEqual({
+      body: {
+        $set: { position: [123, 456] },
+      }
+    })
+  })
+  
+  test('filters objects into arrays', () => {
+    const input = {
+      "body": [{
+        "filter": {
+          "_st": "PUBLIC"
+        },
+        "update": {
+          "$set": {
+            "image.$.replace": {},
+            "image.$.merge": {}
+          },
+          "$unset": {},
+          "$inc": {},
+          "$mul": {},
+          "$currentDate": {},
+          "$push": {
+            "image": {}
+          }
+        }
+      }]
+    } 
+    const result = filterEmptyFormData(input)
+    expect(result).toEqual({
+      body: [{
+       filter: {_st: 'PUBLIC'} 
+      }]
+    })
+  })
+  
+  it('keeps unchanged arrays with null content', () => {
+    const input = { body: { posizione: [null, null], __STATE__: 'DRAFT'}}
+    const result = filterEmptyFormData(input)
+    expect(result).toEqual({
+      body: {
+        posizione: [null, null],
+        __STATE__: 'DRAFT'
       },
+    })
+  })
+  
+  it('does not filter booleans', () => {
+    const input = {
+      body: [{
+        filter: {
+          _st: 'PUBLIC',
+          _id:'ff',
+          creatorId: 'a',
+          createdAt: 'b'
+        },
+        update: {
+          $set: {
+            name:'ff',
+            location:'89'
+          },
+          $unset: {
+            name: true
+          }
+        }
+      }]
     }
+    const result = filterEmptyFormData(input)
+    expect(result).toEqual(input)
+  })
+
+  it('does not filter numbers', () => {
+    const input = {
+      body: [{
+        filter: {
+          _st: 'PUBLIC',
+          _id:'ff',
+          creatorId: 'a',
+          createdAt: 'b'
+        },
+        update: {
+          $set: {
+            name:'ff',
+            location:'89'
+          },
+          $unset: {
+            size: 123
+          }
+        }
+      }]
+    }
+    const result = filterEmptyFormData(input)
+    expect(result).toEqual(input)
   })
 })
--- packages/api-explorer/src/lib/filter-empty-formdata.js
+++ packages/api-explorer/src/lib/filter-empty-formdata.js
@@ -5,16 +5,23 @@
 
 const allUndefined = o => Object.entries(o).values(v => v !== undefined).length <= 0
 const shouldRecurse = (v) => v && typeof v === "object" && !Array.isArray(v) && Object.keys(v).length > 0 
-const shouldDelete = (v) => v === null || v === undefined || Object.keys(v).length <= 0 || allUndefined(v)
- 
+const shouldDelete = (v) => typeof v !== "boolean" && typeof v !== "number" && (v === null || v === undefined || Object.keys(v).length <= 0 || allUndefined(v))
+
 const removeEmpty = obj => {
   Object.keys(obj).forEach(key => {
-    if (shouldRecurse(obj[key])) removeEmpty(obj[key]);
-    if (shouldDelete(obj[key])) delete obj[key];
+    if (Array.isArray(obj[key])) {
+      obj[key].forEach(v => v && removeEmpty(v))
+    } 
+
+    if (shouldRecurse(obj[key])) removeEmpty(obj[key])
+    if (shouldDelete(obj[key])) delete obj[key]
   });
   return obj
-};
+}
 
 export default function filterEmptyFormData(formDataChange) {
+  if (!formDataChange) {
+    return formDataChange
+  }
   return removeEmpty(formDataChange)
 }
