51eb0c2b9ecbfa047168be6f0ba87f74251fe714
Federico Maggi <federico.maggi@mia-platform.eu>
2019-07-24 12:13:02 +0200

    add(A-1123): implemented filtering fn

--- packages/api-explorer/src/lib/filter-empty-formdata.js
+++ packages/api-explorer/src/lib/filter-empty-formdata.js
@@ -2,10 +2,19 @@
  * Copyright © 2019-present Mia s.r.l.
  * All rights reserved
  */
+
+const allUndefined = o => Object.entries(o).values(v => v !== undefined).length <= 0
+const shouldRecurse = (v) => v && typeof v === "object" && !Array.isArray(v) && Object.keys(v).length > 0 
+const shouldDelete = (v) => v === null || v === undefined || Object.keys(v).length <= 0 || allUndefined(v)
+ 
+const removeEmpty = obj => {
+  Object.keys(obj).forEach(key => {
+    if (shouldRecurse(obj[key])) removeEmpty(obj[key]);
+    if (shouldDelete(obj[key])) delete obj[key];
+  });
+  return obj
+};
+
 export default function filterEmptyFormData(formDataChange) {
-  const keys = Object.keys(formDataChange)
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key of keys) {
-    const dataChange = formDataChange[key]
-  }
+  return removeEmpty(formDataChange)
 }
