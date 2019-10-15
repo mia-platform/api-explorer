bbb19d1ca7fc529e2ce0dfcbc416d09453b89214
Riccardo Di Benedetto <riccardo.dibenedetto@mia-platform.eu>
2019-10-15 14:40:01 +0200

    fix port

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './example',
     compress: true,
-    port: 9967,
+    port: 9966,
     hot: true,
     watchContentBase: true,
     before: (app) => {
