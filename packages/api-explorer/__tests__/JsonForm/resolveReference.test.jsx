92a55b23ebb3423ce56380806203bd89d20377d2
Daniele <daniele.cina@makeitapp.eu>
2020-05-13 11:25:31 +0200

    jsonform test name & remove json case

--- packages/api-explorer/__tests__/JsonForm/resolveReference.test.jsx
+++ packages/api-explorer/__tests__/JsonForm/resolveReference.test.jsx
@@ -1,8 +1,8 @@
 import resolveReference from '../../src/JsonForm/resolveReference'
-import SCHEMA_BUG from './resolve-reference-schema-bug.json'
-import EXPECTED_SCHEMA_BUG from './resolve-reference-schema-bug.expected.json'
+import SCHEMA_BUG from './config-root-ref-and-nested.json'
+import EXPECTED_SCHEMA_BUG from './config-root-ref-and-nested.expected.json'
 
-test('resolve correctly', async () => {
+test('resolve correctly with nested $ref', async () => {
   const resp = await resolveReference(SCHEMA_BUG)
   expect(resp).toEqual(EXPECTED_SCHEMA_BUG)
 })
