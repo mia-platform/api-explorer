12472fbe8fee7f9d37cb177e42917e24b108507d
Daniele <daniele.cina@makeitapp.eu>
2020-05-13 11:13:38 +0200

    add example of bug

--- packages/api-explorer/src/JsonForm/resolveReference.js
+++ packages/api-explorer/src/JsonForm/resolveReference.js
@@ -4,6 +4,8 @@ import {omit} from 'ramda'
 export default async function resolveReference (schema) {
   const converted = await parser.dereference(schema)
   // ref: https://github.com/APIDevTools/json-schema-ref-parser/issues/174
+  // if it remains in the root $ ref, the library cannot resolve the other nested references
+  // https://codesandbox.io/s/bug-json-schema-ref-parser-8dehm?file=/src/index.js
   const convertedAgain = await parser.dereference(omit(['$ref'], converted))
   return convertedAgain
 }
