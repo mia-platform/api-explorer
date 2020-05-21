6d5a0dbf0a42f035bbacdf1a0397ec26614ec808
Daniele <daniele.cina@makeitapp.eu>
2020-05-21 09:46:08 +0200

    fix(MMAP-7): repetitions

--- packages/api-explorer/__tests__/Doc.test.jsx
+++ packages/api-explorer/__tests__/Doc.test.jsx
@@ -374,11 +374,14 @@ describe('stripSlash', () => {
 
 test('expand renderCodeAndResponse should be render correctly', () => {
   const element = mountWithIntl(<Doc {...props} />)
-  expect(element.find('.expandable').find(Button).find(FormattedMessage).prop('id')).toEqual('doc.expand')
-  expect(element.find('.expandable').find(Button).prop('icon')).toEqual('import')
-  expect(element.find('.expandable').prop('style')).toMatchSnapshot()
+  const expandableElement = element.find('.expandable')
+  expect(expandableElement.find(Button).find(FormattedMessage).prop('id')).toEqual('doc.expand')
+  expect(expandableElement.find(Button).prop('icon')).toEqual('import')
+  expect(expandableElement.prop('style')).toMatchSnapshot()
   element.find('.expandable').find(Button).simulate('click')
-  expect(element.find('.expandable').find(Button).find(Button).find(FormattedMessage).prop('id')).toEqual('doc.collapse')
-  expect(element.find('.expandable').find(Button).find(Button).prop('icon')).toEqual('export')
-  expect(element.find('.expandable').prop('style')).toMatchSnapshot()
+
+  const expandableElementAfter = element.find('.expandable')
+  expect(expandableElementAfter.find(Button).find(Button).find(FormattedMessage).prop('id')).toEqual('doc.collapse')
+  expect(expandableElementAfter.find(Button).find(Button).prop('icon')).toEqual('export')
+  expect(expandableElementAfter.prop('style')).toMatchSnapshot()
 })
