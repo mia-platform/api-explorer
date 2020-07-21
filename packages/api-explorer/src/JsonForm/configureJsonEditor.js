14c471f25aca1a47b6d40ebd3484caf56830d655
Filippo Rezzonico <filippo.rezzonico@mia-platform.eu>
2020-07-21 11:51:20 +0200

    corrected indentation error

--- packages/api-explorer/src/JsonForm/configureJsonEditor.js
+++ packages/api-explorer/src/JsonForm/configureJsonEditor.js
@@ -23,10 +23,10 @@ function setDefaultCustomization (JSONEditor) {
             document.removeEventListener('click', outsideClickListener)
           }
           document.addEventListener('click', outsideClickListener)
-            this.editjson_textarea.style.width = '450px'
-            this.editjson_textarea.style.height = '340px'
-            this.editjson_holder.style.left = `${parseInt(this.editjson_holder.style.left, 10) -this.editjson_holder.offsetWidth - parseInt(this.editjson_control.style.marginLeft, 10)}px`
-            this.editjson_holder.style.top = `-104px`
+          this.editjson_textarea.style.width = '450px'
+          this.editjson_textarea.style.height = '340px'
+          this.editjson_holder.style.left = `${parseInt(this.editjson_holder.style.left, 10) -this.editjson_holder.offsetWidth - parseInt(this.editjson_control.style.marginLeft, 10)}px`
+          this.editjson_holder.style.top = `-104px`
         }
       }
       postBuild() {
