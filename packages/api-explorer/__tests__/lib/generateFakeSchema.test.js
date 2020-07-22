9950acb2ce9b7b39016fc3b3726e0b80a0c0b89a
danielecina <icloud.d@gmail.com>
2020-07-22 13:13:19 +0200

    change(BMP-379): generate schema example test

--- packages/api-explorer/__tests__/lib/generateFakeSchema.test.js
+++ packages/api-explorer/__tests__/lib/generateFakeSchema.test.js
@@ -9,6 +9,7 @@ describe('generateFakeSchema', () => {
       schema: {
         type: 'object',
         examples: [{ foo: 'bar' }],
+        required: ['foo'],
         properties: {
           foo: { type: 'string' }
         }
@@ -18,9 +19,65 @@ describe('generateFakeSchema', () => {
       }
     },
     {
+      title: 'example of schema from crud-service',
+      schema: {
+        type: 'array',
+        maxItems: 1,
+        items: {
+          type: 'object',
+          required: ['_id', 'creatorId', 'createdAt', 'updaterId', 'updatedAt', '__STATE__'],
+          properties: {
+            _id: {
+              type: 'string',
+              examples: ['000000000000000000000000'],
+              pattern: '^[a-fA-F\\d]{24}$',
+              description: '_id'
+            },
+            creatorId: {
+              type: 'string',
+              description: 'creatorId'
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'createdAt'
+            },
+            updaterId: {
+              type: 'string',
+              description: 'updaterId'
+            },
+            updatedAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'updatedAt'
+            },
+            __STATE__: {
+              type: 'string',
+              description: '__STATE__'
+            }
+          }
+        }
+      },
+      expectation: (generatedFakeSchema) => {
+        // eslint-disable-next-line no-useless-escape
+        const dateTimeRgx = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).([0-9A-Za-z]{1,})/
+        const item = generatedFakeSchema[0]
+        const {_id, creatorId, createdAt, updaterId, updatedAt, __STATE__} = item
+
+        expect(_id).toEqual('000000000000000000000000')
+        expect(creatorId).toEqual(expect.any(String))
+        expect(createdAt.match(dateTimeRgx)).toBeTruthy()
+        expect(updaterId).toEqual(expect.any(String))
+        expect(updatedAt.match(dateTimeRgx)).toBeTruthy()
+        // eslint-disable-next-line no-underscore-dangle
+        expect(__STATE__).toEqual(expect.any(String))
+      }
+    },
+    {
       title: 'use of examples within properties',
       schema: {
         type: 'object',
+        required: ['foo'],
         properties: {
           foo: {
             type: 'string',
@@ -33,10 +90,27 @@ describe('generateFakeSchema', () => {
       }
     },
     {
+      title: 'generated field match pattern',
+      schema: {
+        type: 'object',
+        required: ['foo'],
+        properties: {
+          foo: {
+            type: 'string',
+            pattern: '^[a-fA-F\\d]{24}$'
+          }
+        }
+      },
+      expectation: (generatedFakeSchema, schema) => {
+        expect(generatedFakeSchema.foo.match(schema.properties.foo.pattern)).toBeTruthy()
+      }
+    },
+    {
       title: 'not supported by json-schema-faker - example key outside the properties',
       schema: {
         type: 'object',
         example: {foo: 'bar'},
+        required: ['foo'],
         properties: {
           foo: {
             type: 'string',
@@ -52,6 +126,7 @@ describe('generateFakeSchema', () => {
       title: 'not supported by json-schema-faker - example key inside the properties',
       schema: {
         type: 'object',
+        required: ['foo'],
         properties: {
           foo: {
             type: 'string',
@@ -70,7 +145,7 @@ describe('generateFakeSchema', () => {
     const {title, schema, expectation} = schemaTest
     test(title, () => {
       const result = generateFakeSchema(schema)
-      expectation(result)
+      expectation(result, schema)
     })
   })
 })
