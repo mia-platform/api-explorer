ffb8e0776279f754ed38419515c1066068386fe3
danielecina <icloud.d@gmail.com>
2020-11-12 15:00:06 +0100

    add checks for type error

--- packages/api-explorer/src/JsonForm/arrays-editor.js
+++ packages/api-explorer/src/JsonForm/arrays-editor.js
@@ -12,7 +12,7 @@ module.exports = (classReference) => class arrayCustomEditor extends classRefere
         this.jsoneditor &&
         this.jsoneditor.schema &&
         Array.isArray(this.jsoneditor.schema.required) &&
-        this.jsoneditor.schema.required.includes(this.key) &&
+        this.jsoneditor.schema.required.includes(this.key)
       ) {
         this.header.classList.add('required')
       }
