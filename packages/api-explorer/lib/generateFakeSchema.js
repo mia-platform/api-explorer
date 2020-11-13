4098ad53a3cd521cb87c12cef460b97fe1ff2386
danielecina <icloud.d@gmail.com>
2020-11-13 12:21:06 +0100

    support example keyword for schema

--- packages/api-explorer/lib/generateFakeSchema.js
+++ packages/api-explorer/lib/generateFakeSchema.js
@@ -1,4 +1,4 @@
-import jsf from "json-schema-faker";
+import jsf from "json-schema-faker"
 
 jsf.option({
   failOnInvalidTypes: false,
@@ -6,6 +6,37 @@ jsf.option({
   useDefaultValue: true,
   useExamplesValue: true,
   alwaysFakeOptionals: true,
+})
+
+function generateObjectExample(example, properties) {
+  const generatedExample = { ...properties ? jsf.generate(properties) : {} }
+  // eslint-disable-next-line guard-for-in
+  for (const key in example) {
+    generatedExample[key] = generateExampleFromType(example[key], example[key])
+  }
+  return generatedExample
+}
+
+function generateArrayExample(example) {
+  const generatedExample = []
+  example.forEach(item => {
+    generatedExample.push(generateExampleFromType(item, item))
+  })
+  return generatedExample
+}
+
+function generateExampleFromType (example, schema) {
+  if(Array.isArray(example)) {
+    return generateArrayExample(example)
+  }
+  if (example !== null && !Array.isArray(example) && typeof example === 'object'){
+    return generateObjectExample(example, schema.properties)
+  }
+  return example
+}
+
+jsf.define('example', (example, schema) => {
+  return generateExampleFromType(example, schema)
 })
 
 export default function generateFakeSchema(schema) {
